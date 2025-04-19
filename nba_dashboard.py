dab70a4f65848be07b6a09867bbd437a20e7d0cf
Fouzul Hassan <fouzhas3@gmail.com>
2025-04-19 11:49:17 +0530

    feat: ver 3

--- nba_dashboard.py
+++ nba_dashboard.py
@@ -29,9 +29,10 @@ df_quarters, df_top_teams, df_players_40, df_summary = load_data()
 # --------------------
 st.header("⏱️ Most Scoring Quarter per Team")
 
+print(df_quarters.columns)
 col1, col2 = st.columns([1, 2])
 with col1:
-    st.dataframe(df_quarters)
+    st.dataframe(df_quarters.drop(columns=['Unnamed: 0']))
 with col2:
     fig1 = px.bar(df_quarters, x="Team", y="Score", color="Quarter", barmode="group",
                   title="Highest Scoring Quarter for Each Team")
@@ -44,7 +45,7 @@ st.header("🔥 Top 5 Teams by Total Points Scored")
 
 col3, col4 = st.columns([1, 2])
 with col3:
-    st.dataframe(df_top_teams)
+    st.dataframe(df_top_teams.drop(columns=['Unnamed: 0']))
 with col4:
     fig2 = px.bar(df_top_teams, x="Total_Points", y="Team", orientation="h",
                   title="Top 5 Scoring Teams", text_auto=True)
@@ -73,14 +74,14 @@ st.header("🏁 Team Match Outcomes & Top Scorers")
 
 col7, col8 = st.columns([1, 2])
 with col7:
-    st.dataframe(df_summary)
+    st.dataframe(df_summary.drop(columns=['Unnamed: 0']))
 with col8:
     fig4 = px.bar(df_summary, x="Team", y=["Win", "Loss"], barmode="stack",
                   title="Wins and Losses per Team")
     st.plotly_chart(fig4, use_container_width=True)
 
-st.markdown("### 🎯 Top Scorer per Team")
-st.dataframe(df_summary[['Team', 'Top_Scorer']])
+# st.markdown("### 🎯 Top Scorer per Team")
+# st.dataframe(df_summary[['Team', 'Top_Scorer']])
 
 # --------------------
 # Footer
